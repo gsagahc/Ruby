e1c3a71033f1fede1263124a4de1a3b297ff10e1
Guilherme Chagas <gui@gui-laptop.home>
2017-12-17 18:25:18 -0300

    criação do banco

---
+++ financeiro/app/views/clientes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @cliente, :id, :nome, :cnpj-cpf, :comissao_a_prazo, :endereco, :site, :fone, :celular, :razao_social, :created_at, :updated_at
---
+++ financeiro/db/migrate/20171217210601_create_clientes.rb
@@ -0,0 +1,16 @@
+class CreateClientes < ActiveRecord::Migration
+  def change
+    create_table :clientes do |t|
+      t.string :nome
+      t.string :cnpj-cpf
+      t.decimal :comissao_a_prazo
+      t.string :endereco
+      t.string :site
+      t.string :fone
+      t.string :celular
+      t.string :razao_social
+
+      t.timestamps null: false
+    end
+  end
+end
